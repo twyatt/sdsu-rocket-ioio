e6fc8a13ce8edf8d7261a584a36646048957190b
Travis Wyatt <travis.i.wyatt@gmail.com>
2013-04-13 17:37:42 -0700

    Initial implementation of servo device.

---
+++ sdsu-rocket-control/src/edu/sdsu/rocket/control/devices/PS050.java
@@ -0,0 +1,63 @@
+package edu.sdsu.rocket.control.devices;
+
+import ioio.lib.api.IOIO;
+import ioio.lib.api.PwmOutput;
+import ioio.lib.api.exception.ConnectionLostException;
+
+/**
+ * Servo
+ */
+public class PS050 implements Device {
+
+	private final int PWM_FREQ = 100; // Hz
+	
+	private PwmOutput pwm;
+	private int pwmPin;
+	private int pwmFrequency;
+	private int pulseWidth;
+
+	public PS050(int pwmPin, int pwmFrequency) {
+		this.pwmPin = pwmPin;
+		this.pwmFrequency = pwmFrequency;
+	}
+	
+	@Override
+	public void setup(IOIO ioio) throws ConnectionLostException {
+		pwm = ioio.openPwmOutput(pwmPin, PWM_FREQ);
+	}
+
+	@Override
+	public void loop() throws ConnectionLostException, InterruptedException {
+		// TODO check if lastPulseWidth != pulseWidth to decide if we should call setPulseWidth?
+		pwm.setPulseWidth(pulseWidth);
+		Thread.sleep(100);
+	}
+	
+	/**
+	 * Sets servo position.
+	 * 
+	 * @param percent Between 0 and 100
+	 */
+	public void setPositionPercent(int percent) {
+		setPulseWidth(percent * 2);
+	}
+	
+	/**
+	 * Sets the pulse width (beyond base pulse) to send to the servo.
+	 * 
+	 * @param width Between 0 and 200
+	 */
+	public void setPulseWidth(int width) {
+		pulseWidth = 500 + width;
+	}
+
+	/*
+	 * Device interface methods.
+	 */
+	
+	@Override
+	public String info() {
+		return this.getClass().getSimpleName() + ": rx=" + pwmPin + ", frequency=" + pwmFrequency;
+	}
+	
+}
