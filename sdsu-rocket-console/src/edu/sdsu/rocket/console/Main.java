4d5165391f19898ab762921e9f753048eed24f4e
Travis Wyatt <travis.i.wyatt@gmail.com>
2013-05-06 19:17:14 -0700

    Updated rocket console to auto reconnect.

--- sdsu-rocket-console/src/edu/sdsu/rocket/console/Main.java
+++ sdsu-rocket-console/src/edu/sdsu/rocket/console/Main.java
@@ -1,10 +1,7 @@
 package edu.sdsu.rocket.console;
 
-import java.io.IOException;
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 
-import com.esotericsoftware.kryonet.Client;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
@@ -15,171 +12,75 @@ import edu.sdsu.rocket.Network.LoggingRequest;
 
 public class Main {
 	
-	private static final int CONNECT_TIMEOUT = 10000; // milliseconds
-	private static final int DISCOVER_TIMEOUT = 5000; // milliseconds
-
-	private static Client client = new Client();
-	
-	// TODO change to array
-	private static Runnable runnable;
+	private static final int CONNECT_TIMEOUT = 5000; // milliseconds
+	private static final int DISCOVER_TIMEOUT = 3000; // milliseconds
 
 	public static void main(String[] argv) {
 		// http://code.google.com/p/kryonet/issues/detail?id=29
 //		System.setProperty("java.net.preferIPv4Stack" , "true");
 		
-		setup();
-		go();
-	}
-
-	private static void go() {
-		if (!connect()) {
-			if (!connect("192.168.1.6")) {
-				return;
-			}
-		}
+		String host = (argv.length > 0 ? argv[0] : null);
 		
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			return;
-		}
+		RocketClient client = new RocketClient(200 /* thread sleep (ms) */)
+			.setDiscoverTimeout(DISCOVER_TIMEOUT)
+			.setConnectTimeout(CONNECT_TIMEOUT)
+			.setHost(host)
+			.setUdpPort(Network.UDP_PORT)
+			.setTcpPort(Network.TCP_PORT);
 		
-		LoggingRequest loggingRequest = new LoggingRequest();
-		loggingRequest.enable = true;
-		client.sendTCP(loggingRequest);
-		
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//			return;
-//		}
-//		
-//		System.err.println("Sending authentication key.");
-//		AuthenticationRequest authenticationRequest = new AuthenticationRequest();
-//		authenticationRequest.key = Network.AUTHENTICATION_KEY;
-//		client.sendTCP(authenticationRequest);
-		
-		loop();
-	}
-	
-	private static boolean connect() {
-		System.out.println("Searching for host.");
-		
-		int udpPort = Network.UDP_PORT;
-		InetAddress address = client.discoverHost(udpPort, DISCOVER_TIMEOUT);
-		
-		if (address != null) {
-			return connect(address);
-		} else {
-			System.err.println("Failed to discover host.");
-			return false;
-		}
-	}
-	
-	private static boolean connect(String host) {
-		try {
-			InetAddress inetAddress = InetAddress.getByName(host);
-			return connect(inetAddress);
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-
-	private static boolean connect(InetAddress address) {
-		System.out.println("Connecting to " + address + ".");
-		
-		try {
-			client.connect(CONNECT_TIMEOUT, address, Network.TCP_PORT, Network.UDP_PORT);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		
-		return true;
-	}
-
-	private static void loop() {
-		while (true) {
-			try {
-				if (runnable != null) {
-					runnable.run();
-					runnable = null;
-				}
-				
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-				return;
-			}
-		}
-	}
-
-	private static void setup() {
-		Network.register(client);
-		client.start();
-		
-		client.addListener(new Listener() {
+		client.kryonet.addListener(new Listener() {
 			@Override
 			public void connected(Connection connection) {
 				System.out.println("Connected to " + connection.getRemoteAddressTCP() + ".");
+				
+				System.out.println("Sending request to enable remote logging.");
+				enableRemoteLogging(connection);
+				
+				System.out.println("Sending authentication key.");
+				authenticate(connection);
 			}
 			
 			@Override
 			public void received(Connection connection, Object object) {
-//				System.out.println("received from " + connection.getID());
-				
 				if (object instanceof LogMessage) {
-					LogMessage log = (LogMessage)object;
-					
-					if (log.level >= Network.LOG_LEVEL_ERROR) {
-						System.err.println("ERROR: " + log.message);
-					} else {
-						System.out.println("LOG: " + log.message);
-					}
+					onLogMessage((LogMessage)object);
 				}
 			}
 			
 			@Override
 			public void disconnected(Connection connection) {
 				System.out.println("Disconnected.");
-				reconnect();
 			}
 		});
-	}
-	
-	private static void reconnect() {
-		postRunnable(new Runnable() {
-			@Override
-			public void run() {
-				System.out.println("Reconnecting ...");
-				
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				go();
-				
-//				int timeout = 30000; // milliseconds
-//				
-//				try {
-//					System.out.println("Reconnecting ...");
-//					client.reconnect(timeout);
-//				} catch (IOException e) {
-//					e.printStackTrace();
-//					return;
-//				}
+		
+		while (true) {
+			try {
+				client.connect();
+			} catch (UnknownHostException e) {
+				break;
 			}
-		});
+			client.loop();
+		}
 	}
 
-	public static void postRunnable(Runnable runnable) {
-		Main.runnable = runnable;
+	protected static void onLogMessage(LogMessage log) {
+		if (log.level >= Network.LOG_LEVEL_ERROR) {
+			System.err.println("ERROR: " + log.message);
+		} else {
+			System.out.println("LOG: " + log.message);
+		}
+	}
+
+	protected static void authenticate(Connection connection) {
+		AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+		authenticationRequest.key = Network.AUTHENTICATION_KEY;
+		connection.sendTCP(authenticationRequest);
+	}
+
+	protected static void enableRemoteLogging(Connection connection) {
+		LoggingRequest loggingRequest = new LoggingRequest();
+		loggingRequest.enable = true;
+		connection.sendTCP(loggingRequest);
 	}
 	
 }
---
+++ sdsu-rocket-console/src/edu/sdsu/rocket/console/RocketClient.java
@@ -0,0 +1,135 @@
+package edu.sdsu.rocket.console;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import com.esotericsoftware.kryonet.Client;
+
+import edu.sdsu.rocket.Network;
+import edu.sdsu.rocket.Network.LoggingRequest;
+
+public class RocketClient {
+
+	public Client kryonet = new Client();
+	
+	private String manualHost;
+	private int tcpPort;
+	private int udpPort;
+	
+	private int discoverTimeout;
+	private int connectTimeout;
+	
+	/**
+	 * Thread loop sleep duration (milliseconds).
+	 */
+	private long sleep;
+	
+	public RocketClient(int threadSleep) {
+		sleep = threadSleep;
+		start();
+	}
+	
+	/**
+	 * Sets the discover host timeout.
+	 * 
+	 * @param discoverTimeout Discover timeout in seconds.
+	 * @return
+	 */
+	public RocketClient setDiscoverTimeout(int discoverTimeout) {
+		this.discoverTimeout = discoverTimeout;
+		return this;
+	}
+	
+	/**
+	 * Sets the connect timeout.
+	 * 
+	 * @param connectTimeout Connect timeout in seconds.
+	 * @return
+	 */
+	public RocketClient setConnectTimeout(int connectTimeout) {
+		this.connectTimeout = connectTimeout;
+		return this;
+	}
+	
+	public RocketClient setHost(String host) {
+		this.manualHost = host;
+		return this;
+	}
+	
+	public RocketClient setTcpPort(int tcpPort) {
+		this.tcpPort = tcpPort;
+		return this;
+	}
+	
+	public RocketClient setUdpPort(int udpPort) {
+		this.udpPort = udpPort;
+		return this;
+	}
+	
+	public void start() {
+		Network.register(kryonet);
+		kryonet.start();
+	}
+	
+	public void stop() {
+		kryonet.stop();
+	}
+	
+	public boolean connect() throws UnknownHostException {
+		InetAddress address;
+		
+		if (manualHost != null) {
+			address = InetAddress.getByName(manualHost);
+			
+			try {
+				System.out.println("Connecting to " + address + ".");
+				kryonet.connect(connectTimeout, address, tcpPort);
+				if (kryonet.isConnected()) {
+					return true;
+				} else {
+					System.err.println("Failed to connect to " + address + ".");
+				}
+			} catch (IOException e) {
+				// ignore
+			}
+		}
+		
+		System.out.println("Discovering host.");
+		address = kryonet.discoverHost(udpPort, discoverTimeout);
+		if (address == null) {
+			System.err.println("Failed to discover host.");
+			return false;
+		}
+		
+		try {
+			System.out.println("Connecting to " + address + ".");
+			kryonet.connect(connectTimeout, address, tcpPort);
+		} catch (IOException e) {
+			System.err.println("Failed to connect to " + address + ".");
+		}
+	
+		return kryonet.isConnected();
+	}
+
+	public void enableRemoteLogging() {
+		LoggingRequest loggingRequest = new LoggingRequest();
+		loggingRequest.enable = true;
+		kryonet.sendTCP(loggingRequest);
+	}
+	
+	/**
+	 * Blocks until connection is lost.
+	 */
+	public void loop() {
+		while (kryonet.isConnected()) {
+			try {
+				Thread.sleep(sleep);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				return;
+			}
+		}
+	}
+	
+}
